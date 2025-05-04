1bfa326d4b5ac4e974b0b6431b98f1afb7e51e4c
Samuel Hríbik <xhribi00@stud.fit.vutbr.cz>
2025-05-04 19:17:39 +0300

    Few minor changes and new controllers

---
+++ HotelManagement/Controllers/EventsController.cs
@@ -0,0 +1,23 @@
+﻿using HotelManagement.DAL.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelManagement.Controllers
+{
+    public class EventsController : Controller
+    {
+        private readonly ILogger<EventsController> _logger;
+        private readonly EventRepository _eventRepository;
+
+        public EventsController(ILogger<EventsController> logger, EventRepository eventRepository)
+        {
+            _logger = logger;
+            _eventRepository = eventRepository;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var events = await _eventRepository.GetAllAsync();
+            return View(events);
+        }
+    }
+}
--- HotelManagement/DAL/Repositories/IRepository.cs
+++ HotelManagement/DAL/Repositories/IRepository.cs
@@ -5,6 +5,7 @@ namespace HotelManagement.DAL.Repositories
     public interface IRepository<TModel> where TModel : IModel
     {
         IList<TModel> GetAll();
+        Task<IList<TModel>> GetAllAsync();
         TModel? GetById(Guid id);
         Guid Create(TModel model);
         Guid? Update(TModel model);
