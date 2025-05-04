6cc07c31363191b2e29709bcaac3e9960135fac2
Sebastián Chupáč <sebastianchupac@gmail.com>
2025-05-04 19:09:31 +0300

    Added FullCalendar js component to Room detail. Updated Room model with enum RoomType. Updated Room seeds and RoomList view. Added RoomTypeHelper.

--- HotelManagement/DAL/Seeds/RoomSeeds.cs
+++ HotelManagement/DAL/Seeds/RoomSeeds.cs
@@ -1,4 +1,5 @@
-﻿using HotelManagement.Models;
+﻿using HotelManagement.Enums;
+using HotelManagement.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace HotelManagement.DAL.Seeds
@@ -10,22 +11,25 @@ namespace HotelManagement.DAL.Seeds
         {
             Id = Guid.Parse("a242ae42-b5f3-417e-9277-b89ac16fca21"),
             RoomNumber = "101",
-            Description = "Deluxe Room with sea view"
-        };
+            Description = "Deluxe Room with sea view",
+			RoomType = RoomType.Single
+		};
 
         public static readonly Room Room2 = new Room
         {
             Id = Guid.Parse("a242ae42-b5f3-417e-9277-b89ac16fca22"),
             RoomNumber = "102",
-            Description = "Standard Room with garden view"
-        };
+            Description = "Standard Room with garden view",
+			RoomType = RoomType.Double
+		};
 
         public static readonly Room Room3 = new Room
         {
             Id = Guid.Parse("a242ae42-b5f3-417e-9277-b89ac16fca23"),
             RoomNumber = "103",
-            Description = "Suite with balcony"
-        };
+            Description = "Suite with balcony",
+			RoomType = RoomType.Suite
+		};
 
         public static void Seed(ModelBuilder modelBuilder)
         {
---
+++ HotelManagement/Helpers/RoomTypeHelper.cs
@@ -0,0 +1,21 @@
+﻿using HotelManagement.Enums;
+
+namespace HotelManagement.Helpers
+{
+	public static class RoomTypeHelper
+	{
+		public static readonly Dictionary<RoomType, (string Name, string Description, int Capacity)> Details =
+			new()
+			{
+			{ RoomType.Single, ("Single Room", "A cozy room for one person", 1) },
+			{ RoomType.Double, ("Double Room", "Comfortable for two guests", 2) },
+			{ RoomType.Suite, ("Suite", "Luxury suite with extra amenities", 4) },
+			{ RoomType.Familly, ("Family Room", "Spacious room for families", 6) }
+			};
+
+		public static string GetName(RoomType type) => Details[type].Name;
+		public static string GetDescription(RoomType type) => Details[type].Description;
+		public static int GetCapacity(RoomType type) => Details[type].Capacity;
+	}
+
+}
